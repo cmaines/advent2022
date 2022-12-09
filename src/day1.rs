405240496f04caf597218b1283ccb4634126224b
Chris Maines <cheetos1@gmail.com>
2022-12-09 02:49:16 -0800

    Making day 1 more idiomatic

--- src/day1.rs
+++ src/day1.rs
@@ -2,17 +2,16 @@ use sorted_vec::SortedVec;
 
 pub fn day_1() {
     let mut counts = SortedVec::new();
-    
-    if let Ok(lines) = read_lines("input1.txt") {
+    if let Ok(lines) = super::utils::read_lines("input1.txt") {
         let mut curr = 0;
         for line in lines {
-            if let Ok(str) = line {
-                if str == "" {
+            match line.as_deref() {
+                Ok("") =>  {
                     counts.insert(curr);
                     curr = 0;
-                } else if let Ok(num) = str.parse::<u32>() {
-                    curr += num;
-                }
+                },
+                Ok(str) => curr += str.parse::<u32>().unwrap(),
+                _ => panic!("Unknown string")
             }
         }
         counts.insert(curr);
@@ -21,9 +20,6 @@ pub fn day_1() {
         }
     }
     
-    let mut total = 0;
-    for num in counts.iter() {
-        total += num;
-    }
-    println!("Calories are {:?}", total);
+    let total: u32 = counts.iter().sum();
+    println!("Calories are {}", total);
 }
